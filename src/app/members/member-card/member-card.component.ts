0dcaa0f92ced7a5c6c12e053066a4ff3f43ff526
Or Minzberg <shacker7@gmail.com>
2018-02-24 19:02:47 +0200

    Message-System
    Designing the message tab chat system

--- src/app/members/member-card/member-card.component.ts
+++ src/app/members/member-card/member-card.component.ts
@@ -21,7 +21,7 @@ export class MemberCardComponent implements OnInit {
 
   sendLike(id: number) {
     this.userSerivce.sendLike(this.authService.decodedToken.nameid, id).subscribe(data => {
-      this.alertify.success('You have liked' + this.user.knownAs);
+      this.alertify.success('You have liked ' + this.user.knownAs);
     }, error => {
       this.alertify.error(error);
     });
